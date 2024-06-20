c251c698f4c9bea599d463bbf0c56ce14f34ede3
Sveta Lantsman <malishkin2002@gmail.com>
2024-06-20 12:28:01 +0300

    using Namespaces

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f807f92a9b57570c32b041f9810e8861335966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5dc6573e2993fa319e9d006686083c15db7fdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
